827f400473ada2beb34f70db44d49554bd1d488b
donnwilzy <ask4godswill@gmail.com>
2024-06-15 15:15:23 +0100

    facebook passport strategy

---
+++ config/facebookAuth.js
@@ -0,0 +1,21 @@
+const passport = require('passport');
+const FacebookStrategy = require('passport-facebook').Strategy;
+const facebookConfig = require('./facebookConfig');
+
+passport.use(new FacebookStrategy({
+  clientID: facebookConfig.FACEBOOK_APP_ID,
+  clientSecret: facebookConfig.FACEBOOK_APP_SECRET,
+  callbackURL: facebookConfig.CALLBACK_URL,
+  profileFields: ['id', 'emails', 'name', 'picture.type(large)']
+}, (accessToken, refreshToken, profile, cb) => {
+  // Authenticate user without connecting to DB
+  cb(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+  done(null, { _id: id });
+});
