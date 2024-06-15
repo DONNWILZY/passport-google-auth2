135852591737f62ce3ffbd2e4c1be10ab3c089c6
donnwilzy <ask4godswill@gmail.com>
2024-06-15 14:53:08 +0100

    index js adjutsed

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
-//index.js
-
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const session = require('express-session')
-const mongoose  = require('mongoose');
-const expressSession = require('express-session');
-const jwt = require('jsonwebtoken');
+const session = require('express-session');
+const mongoose = require('mongoose');
 const passport = require('passport');
 const errorHandler = require('./middlewares/errorHandler');
 
 dotenv.config();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(errorHandler); // error handler middleware
-app.use(session({ secret: 'dog'}));
+app.use(errorHandler);
+app.use(session({ secret: 'dog', resave: false, saveUninitialized: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 const connectToDatabase = require('./config/db');
-connectToDatabase(); // database function
+connectToDatabase();
 
-app.get('/', (req, res) =>{
-    res.send( `DEFAULT ROUTE IS WORKING`)
+app.get('/', (req, res) => {
+  res.send('DEFAULT ROUTE IS WORKING');
 });
-
 
 // Import routes
 const auth = require('./routes/authRoute');
@@ -33,9 +28,6 @@ const auth = require('./routes/authRoute');
 // Routes middlewares
 app.use('/api/auth', auth);
 
-
-
-
-app.listen(PORT, ()=>{
-  console.log(`Connected on PORT ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Connected on PORT ${PORT}`);
+});
